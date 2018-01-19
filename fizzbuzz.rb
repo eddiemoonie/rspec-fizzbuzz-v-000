8c2987de5c697875ca5b7bb46e3c0fb8e69c4f7a
Eddie Moon <eddiemoonie@gmail.com>
2018-01-19 20:37:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
+    "Buzz"
   elsif int % 15 == 0
-    puts "FizzBuzz"
-  else
-    puts "nil"
+    "FizzBuzz"
+  end
 end
